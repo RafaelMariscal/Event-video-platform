f6d77a0eaa1c84d36d71682dc333396eca7e1e66
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-06-26 19:31:00 -0300

    Feat/Added scrollbar style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,11 @@ export function Sidebar() {
   const { data } = useGetLessonsQuery()
 
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 boder-l border-gray-600">
-      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+    <aside className="w-[480px] max-h-screen overflow-y-auto bg-gray-700 p-6 boder-l border-gray-600">
+      <span className="font-bold text-[2rem] pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de Aulas
       </span>
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 text-[1.15rem]">
         {data?.lessons.map(lesson => {
           return (
             <Lesson
